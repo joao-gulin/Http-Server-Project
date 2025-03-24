e1b99387ccf48396988a30375ad55ff8862340c6
joao-gulin <joaogulin@gmail.com>
2025-03-24 01:39:14 +0000

    socket implementation

--- webserver.c
+++ webserver.c
@@ -1,5 +1,14 @@
+#include <errno.h>
 #include <stdio.h>
+#include <sys/socket.h>
+
 int main() {
-  printf("hello world \n");
+  // Create a socket
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == -1) {
+    perror("webserver (socket)");
+    return 1;
+  }
+  printf("socket created successfully \n");
   return 0;
 }
