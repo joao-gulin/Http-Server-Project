20cdc9237c229040a21d7ce5908b095917f5aca7
joao-gulin <joaogulin@gmail.com>
2025-03-24 01:47:42 +0000

    socket bounding to the address

--- webserver.c
+++ webserver.c
@@ -1,7 +1,9 @@
-#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/socket.h>
 
+#define PORT 8080
 int main() {
   // Create a socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -10,5 +12,20 @@ int main() {
     return 1;
   }
   printf("socket created successfully \n");
+
+  // Create the address to bind the socket to
+  struct sockaddr_in host_addr;
+  int host_addrlen = sizeof(host_addr);
+
+  host_addr.sin_family = AF_INET;
+  host_addr.sin_port = htons(PORT);
+  host_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  // Bind socket to the address
+  if (bind(sockfd, (struct sockaddr *)&host_addr, host_addrlen) != 0) {
+    perror("webserver (bind)");
+    return 1;
+  }
+  printf("socket successfully bound to address\n");
   return 0;
 }
